4a2502f489a0a3ccc5b392879a1ccb75777fd7fb
Daniel <106247186+DanThePythonMan@users.noreply.github.com>
2023-07-14 15:16:55 +0100

    Complete Bugfix for blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -10,7 +10,7 @@ var hidden;
 var deck;
 
 var canHit = true;
-
+//--------------------------------------------------------------------------------------------------
 window.onload = function () {
   buildDeck();
   shuffleDeck();
@@ -18,7 +18,7 @@ window.onload = function () {
   document.getElementById("hit").addEventListener("click", hit);
   document.getElementById("stand").addEventListener("click", stand);
 };
-
+//--------------------------------------------------------------------------------------------------
 function buildDeck() {
   let values = [
     "A",
@@ -44,7 +44,7 @@ function buildDeck() {
     }
   }
 }
-
+//--------------------------------------------------------------------------------------------------
 function shuffleDeck() {
   for (let i = 0; i < deck.length; i++) {
     let j = Math.floor(Math.random() * deck.length);
@@ -52,9 +52,9 @@ function shuffleDeck() {
     deck[i] = deck[j];
     deck[j] = temp;
   }
-  console.log(deck);
+  //console.log(deck);
 }
-
+//--------------------------------------------------------------------------------------------------
 function startGame() {
   hidden = deck.pop();
   dealerSum += getValue(hidden);
@@ -62,12 +62,12 @@ function startGame() {
   let cardImg = document.createElement("img");
   let card = deck.pop();
   cardImg.src = "./cards/" + card + ".png";
-  document.getElementById("dealerCards").appendChild(cardImg);
   dealerSum += getValue(card);
   dealerAceCount += checkAce(card);
-
   dealerSum = reduceAce(dealerSum, dealerAceCount);
-  console.log("Dealer Sum:" + dealerSum);
+  document.getElementById("dealerCards").append(cardImg);
+
+  //console.log("Dealer Sum:" + dealerSum);
   document.getElementById("dealerSum").innerText =
     (dealerSum - getValue(hidden)).toString() + " + Unknown";
 
@@ -81,9 +81,9 @@ function startGame() {
   }
   document.getElementById("playerSum").innerText = playerSum;
 
-  console.log("Player Sum:" + playerSum);
+  //console.log("Player Sum:" + playerSum);
 }
-
+//--------------------------------------------------------------------------------------------------
 function hit() {
   if (!canHit) {
     return;
@@ -96,22 +96,26 @@ function hit() {
   playerAceCount += checkAce(card);
   document.getElementById("playerCards").append(cardImg);
 
-  if (reduceAce(playerSum, playerAceCount) > 21) {
-    canHit = false;
+  if (playerSum > 21 && playerAceCount > 0) {
+    playerSum = reduceAce(playerSum, playerAceCount);
+    playerAceCount--;
   }
-  playerSum = reduceAce(playerSum, playerAceCount);
-  document.getElementById("playerSum").innerText = playerSum;
+
   if (playerSum > 21) {
     stand();
   }
-}
 
+  document.getElementById("playerSum").innerText = playerSum;
+  //console.log(playerSum, playerAceCount);
+}
+//--------------------------------------------------------------------------------------------------
 function stand() {
-  dealerSum = reduceAce(dealerSum, dealerAceCount);
-  playerSum = reduceAce(playerSum, playerAceCount);
+  canHit = false;
+  document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+
+  let message = "";
   if (playerSum > 21) {
-    document.getElementById("results").innerText = "You Lose, You Bust";
-    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+    message = "You Lose, you went bust.";
   } else {
     while (dealerSum < 17) {
       let cardImg = document.createElement("img");
@@ -119,17 +123,11 @@ function stand() {
       cardImg.src = "./cards/" + card + ".png";
       dealerSum += getValue(card);
       dealerAceCount += checkAce(card);
-
+      dealerSum = reduceAce(dealerSum, dealerAceCount);
       document.getElementById("dealerCards").append(cardImg);
     }
 
-    canHit = false;
-    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
-
-    let message = "";
-    if (playerSum > 21) {
-      message = "You Lose, you went bust.";
-    } else if (dealerSum > 21 && playerSum <= 21) {
+    if (dealerSum > 21) {
       message = "You Win, the dealer went bust and you didn't.";
     } else if (playerSum == dealerSum) {
       message = "Dealer Wins, you tied!";
@@ -138,21 +136,22 @@ function stand() {
     } else if (playerSum < dealerSum) {
       message = "You Lose, the dealer got more than you.";
     }
-
-    if (!hasStood) {
-      document.getElementById("dealerSum").innerText = dealerSum;
-      document.getElementById("results").innerText = message;
-      container = document.getElementById("refreshContainer");
-      var retryButton = document.createElement("button");
-      retryButton.innerText = "Retry";
-      retryButton.className = "button";
-      retryButton.addEventListener("click", refresh);
-      container.appendChild(retryButton);
-    }
-    hasStood = true;
   }
+
+  if (!hasStood) {
+    document.getElementById("dealerSum").innerText = dealerSum;
+    document.getElementById("results").innerText = message;
+    container = document.getElementById("refreshContainer");
+    var retryButton = document.createElement("button");
+    retryButton.innerText = "Retry";
+    retryButton.className = "button";
+    retryButton.addEventListener("click", refresh);
+    container.appendChild(retryButton);
+  }
+  hasStood = true;
 }
 
+//--------------------------------------------------------------------------------------------------
 function getValue(card) {
   let data = card.split("-");
   let value = data[0];
@@ -165,14 +164,14 @@ function getValue(card) {
   }
   return parseInt(value);
 }
-
+//--------------------------------------------------------------------------------------------------
 function checkAce(card) {
   if (card[0] == "A") {
     return 1;
   }
   return 0;
 }
-
+//--------------------------------------------------------------------------------------------------
 function reduceAce(playerSum, playerAceCount) {
   while (playerSum > 21 && playerAceCount > 0) {
     playerSum -= 10;
@@ -180,7 +179,8 @@ function reduceAce(playerSum, playerAceCount) {
   }
   return playerSum;
 }
-
+//--------------------------------------------------------------------------------------------------
 function refresh() {
   location.reload();
 }
+//--------------------------------------------------------------------------------------------------
